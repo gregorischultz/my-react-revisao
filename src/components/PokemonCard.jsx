c518d65e2e79a9b6d0ec27536e9231dd748a7f00
Gregori Schultz <gregorischultz07@gmail.com>
2024-05-20 17:22:26 +0200

    React Basics 03 - Pokédex - Afficher des données dans un composant

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,31 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 function PokemonCard () {
+    const pokemon = pokemonList[1];
+    
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
+        {pokemon.imgSrc != null ? ( //aqui eu coloco a condiçao que se a imagem do pokemon for  diferente de null ou img existente, voce vai mostrar tal imagem
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : ( // e aqui os dois pontos vai significar "se nao" mostre "???"
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
     )
 }
 
