cddcdd9cf2c8ce24a7236f2f390b7a50a7f51d08
Gregori Schultz <gregorischultz07@gmail.com>
2024-05-20 16:47:13 +0200

    React Basics 02 - Pokédex - Ton premier composant

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PokemonCard () {
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+            <figcaption>Bulbasaur</figcaption>
+        </figure>
+    )
+}
+
+export default PokemonCard;
