4b2c45d56941b8c8b66a03314500f41cf5f00596
Gregori Schultz <gregorischultz07@gmail.com>
2024-05-21 16:56:08 +0200

    React Basics 05 proptypes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import PropTypes from "prop-types";
 
 
 
 
 
 function PokemonCard ({ pokemon }) {
-    
     return (
         <figure>
         {pokemon.imgSrc != null ? ( //aqui eu coloco a condiçao que se a imagem do pokemon for  diferente de null ou img existente, voce vai mostrar tal imagem
@@ -17,6 +17,13 @@ function PokemonCard ({ pokemon }) {
         <figcaption>{pokemon.name}</figcaption>
       </figure>
     )
+}
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape ({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    }).isRequired
 }
 
 export default PokemonCard;
